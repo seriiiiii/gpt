7e9e9624966c23274e21d4796a6c9fbde900bf96
[디지캡] 박세리 <srpark@digicaps.com>
2024-04-02 14:39:02 +0900

    css. 수정

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-var modal = document.getElementById("Modal");
-var icon = document.querySelector(".settings-icon");
-var span = document.getElementsByClassName("close")[0];
 $(document).ready(function() {
+  // 스위치 토글 이벤트 설정
   $('.switch-toggle').change(function() {
     var $messageContent = $(this).closest('.bot-message').find('.message-content');
     if (this.checked) {
@@ -10,65 +8,63 @@ $(document).ready(function() {
       $messageContent.removeClass('dark-mode');
     }
   });
-});
 
-icon.onclick = function() {
-  modal.style.display = "block";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-$(document).ready(function() {
+  // 서브 메뉴 토글 이벤트 설정
   $("#toggleSubMenu").click(function() {
     $(".hide_menu").toggle();
-    
     $(".container").toggleClass("sub-menu-open");
-    
+
     if ($(".hide_menu").css("display") === "none") {
-      $(".container").css("margin-left", "70px");
+      $(".container").css("margin-left", "180px");
     } else {
       $(".container").css("margin-left", "320px");
     }
   });
 
-  $(".left.side-menu .hashtag").click(function() {
-    $(".left_sub_menu").show();
-    $(".left_sub_menu_2").hide();
+  // 수집 버튼 클릭 이벤트 설정
+  $("#collectButton").click(function() {
+    var botMessages = document.querySelectorAll(".bot-message");
+    var collectMessages = document.querySelectorAll(".collect_message");
+
+    botMessages.forEach(function(message) {
+      message.style.display = "none";
+      collectButton.style.display = "none";
+    });
+
+    collectMessages.forEach(function(message) {
+      message.style.display = "flex"; 
+    });
   });
 
-  $(".left.side-menu .comment").click(function() {
-    $(".left_sub_menu_2").show();
-    $(".left_sub_menu").hide();
+  // 메인 타이틀 클릭 이벤트 설정
+  $('.main_title').click(function() {
+    $(this).children('.sub-title').slideToggle();
+    $(this).find('.arrow').toggleClass('rotate');
+  });
+
+  // 네비게이션 플로드 버튼 클릭 이벤트 설정
+  $('.nav_fold_btn').click(function () {
+    $('body').toggleClass('nav_fold');
+  });
+
+  // 설정 아이콘과 툴팁 관련 이벤트 설정
+  $('.setting').hover(function() {
+    // 마우스 호버 인 시 설정 아이콘 옆에 툴팁 표시
+    $(this).next('.tooltip').show();
+  }, function() {
+    // 마우스가 벗어날 때 툴팁 숨김
+    $(this).next('.tooltip').hide();
   });
 });
-document.getElementById("collectButton").addEventListener("click", function() {
-  var botMessages = document.querySelectorAll(".bot-message");
-  var collectMessages = document.querySelectorAll(".collect_message");
 
-  botMessages.forEach(function(message) {
-    message.style.display = "none";
-    collectButton.style.display = "none";
-  });
-
-  collectMessages.forEach(function(message) {
-    message.style.display = "flex"; 
-  });
-});
+// 동적 최대 높이 설정
 window.onload = function() {
   setDynamicMaxHeight();
 };
 
 function setDynamicMaxHeight() {
   var windowHeight = window.innerHeight;
-  var containerHeight = windowHeight - 100; 
+  var containerHeight = windowHeight - 10; 
 
   var messageContents = document.querySelectorAll('.message-content'); 
 
@@ -77,85 +73,23 @@ function setDynamicMaxHeight() {
   });
 }
 
-$(document).ready(function() {
-  $('.main_title').click(function() {
-    $(this).children('.sub-title').slideToggle();
-    $(this).find('.arrow').toggleClass('rotate');
-  });
-});
-
-function enableEdit() {
-  var editableHeading = document.querySelector('.editable-heading');
-  var editButton = editableHeading.querySelector('.edit-button');
-  var saveButton = editableHeading.querySelector('.save-button');
-  var headingText = editableHeading.querySelector('span');
-  var headingInput = editableHeading.querySelector('input[type="text"]');
-  var hashTagForm = document.querySelector('.hash-tag-form');
-
-  editButton.style.display = 'none';
-  saveButton.style.display = 'inline-block';
-  headingText.style.display = 'none';
-  headingInput.style.display = 'inline-block';
-  hashTagForm.style.display = 'block';
-  headingInput.readOnly = false;
-  headingInput.focus(); 
-  hashTagForm.style.display = 'block';
+// 다크 모드 토글
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
 }
 
-function saveEdit() {
-  var editableHeading = document.querySelector('.editable-heading');
-  var editButton = editableHeading.querySelector('.edit-button');
-  var saveButton = editableHeading.querySelector('.save-button');
-  var headingText = editableHeading.querySelector('span');
-  var headingInput = editableHeading.querySelector('input[type="text"]');
-  var hashTagForm = document.querySelector('.hash-tag-form');
-
-  editButton.style.display = 'inline-block';
-  saveButton.style.display = 'none';
-  headingText.style.display = 'inline-block';
-  headingInput.style.display = 'none';
-  hashTagForm.style.display = 'none'; 
-}
-
-function handleKeyPress(event) {
-  const keyCode = event.keyCode || event.which;
-  if (keyCode === 13) {
-      event.preventDefault();
-      createHashTagButton(event.target.value);
-      event.target.value = ''; 
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-}
-
-function createHashTagButton(text) {
-  const hashTagForm = document.querySelector('.hash-tag-form');
-  const hashTagList = document.createElement('div');
-  hashTagList.className = 'hash-tag-list';
-
-  const hashTagButton = document.createElement('button');
-  hashTagButton.innerText = '#' + text;
-
-  const deleteButton = document.createElement('span');
-  deleteButton.innerText = 'x';
-  deleteButton.className = 'delete-tag';
-  deleteButton.addEventListener('click', function() {
-      hashTagList.removeChild(hashTagButton);
-      hashTagList.removeChild(deleteButton);
-      repositionButtons(hashTagList);
-  });
-
-  hashTagButton.appendChild(deleteButton);
-  hashTagList.appendChild(hashTagButton);
-  hashTagForm.insertAdjacentElement('beforebegin', hashTagList);
-  repositionButtons(hashTagList);
-}
-
-function repositionButtons(hashTagList) {
-  const buttons = hashTagList.querySelectorAll('button');
-  const deleteButtons = hashTagList.querySelectorAll('.delete-tag');
-
-  for (let i = 0; i < buttons.length; i++) {
-      buttons[i].style.order = i;
-      deleteButtons[i].style.order = i;
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
+document.getElementById("conversation").style.display = "none";
