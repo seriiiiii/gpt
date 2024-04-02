9abf2f86c54c51eab48a3e893243772c5b8e5402
[디지캡] 박세리 <srpark@digicaps.com>
2024-04-02 17:26:42 +0900

    코드 정렬 정리

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // 스위치 토글 이벤트 설정
-  $('.switch-toggle').change(function() {
-    var $messageContent = $(this).closest('.bot-message').find('.message-content');
+  $(".switch-toggle").change(function () {
+    var $messageContent = $(this)
+      .closest(".bot-message")
+      .find(".message-content");
     if (this.checked) {
-      $messageContent.addClass('dark-mode');
+      $messageContent.addClass("dark-mode");
     } else {
-      $messageContent.removeClass('dark-mode');
+      $messageContent.removeClass("dark-mode");
     }
   });
 
   // 서브 메뉴 토글 이벤트 설정
-  $("#toggleSubMenu").click(function() {
+  $("#toggleSubMenu").click(function () {
     $(".hide_menu").toggle();
     $(".container").toggleClass("sub-menu-open");
 
@@ -22,55 +24,58 @@ $(document).ready(function() {
   });
 
   // 수집 버튼 클릭 이벤트 설정
-  $("#collectButton").click(function() {
+  $("#collectButton").click(function () {
     var botMessages = document.querySelectorAll(".bot-message");
     var collectMessages = document.querySelectorAll(".collect_message");
 
-    botMessages.forEach(function(message) {
+    botMessages.forEach(function (message) {
       message.style.display = "none";
       collectButton.style.display = "none";
     });
 
-    collectMessages.forEach(function(message) {
-      message.style.display = "flex"; 
+    collectMessages.forEach(function (message) {
+      message.style.display = "flex";
     });
   });
 
-  $('.main_title').click(function() {
-    $(this).children('.sub-title').slideToggle();
-    $(this).find('.arrow').toggleClass('rotate');
+  $(".main_title").click(function () {
+    $(this).children(".sub-title").slideToggle();
+    $(this).find(".arrow").toggleClass("rotate");
   });
 
-  $('.nav_fold_btn').click(function () {
-    $('body').toggleClass('nav_fold');
+  $(".nav_fold_btn").click(function () {
+    $("body").toggleClass("nav_fold");
   });
 
-  $('.setting').hover(function() {
-    $(this).next('.tooltip').show();
-  }, function() {
-    $(this).next('.tooltip').hide();
-  });
+  $(".setting").hover(
+    function () {
+      $(this).next(".tooltip").show();
+    },
+    function () {
+      $(this).next(".tooltip").hide();
+    }
+  );
 });
 
 // 동적 최대 높이 설정
-window.onload = function() {
+window.onload = function () {
   setDynamicMaxHeight();
 };
 
 function setDynamicMaxHeight() {
   var windowHeight = window.innerHeight;
-  var containerHeight = windowHeight - 10; 
+  var containerHeight = windowHeight - 10;
 
-  var messageContents = document.querySelectorAll('.message-content'); 
+  var messageContents = document.querySelectorAll(".message-content");
 
-  messageContents.forEach(function(content) {
-    content.style.maxHeight = containerHeight + 'px';
+  messageContents.forEach(function (content) {
+    content.style.maxHeight = containerHeight + "px";
   });
 }
 
 // 다크 모드 토글
 function toggleDarkMode() {
-  document.body.classList.toggle('dark-mode');
+  document.body.classList.toggle("dark-mode");
 }
 
 function openTab(evt, tabName) {
